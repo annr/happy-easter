72417d25e459dfe414b0d955ee7a400b607f0693
Ann Robson <nosbora@gmail.com>
2019-01-06 15:33:20 -0800

    Adding emojis for winter holidays

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 exports.printBeautifulPaganHolidaySymbols = function() {
   console.log("🌷🐰🌷🐰🌷🐰🌷🐰🌷🐰🌷🐰🌷🐰🌷🐰🌷🐰🌷🐰🌷🐰🌷🐰🌷");
 }
+
 exports.printBeautifulGreetingCardHolidaySymbols = function() {
   console.log("🌹💝💌🌹💝💌🌹💝💌🌹💝💌🌹💝💌🌹💝💌🌹💝💌🌹💝💌🌹");
+}
+
+exports.printBeautifulPaganHolidaySymbolsWinter = function() {
+  console.log("🌲❄️ 🌲❄️ 🌲❄️ 🌲❄️ 🌲❄️ 🌲❄️ 🌲❄️ 🌲❄️ 🌲❄️ 🌲❄️ 🌲❄️ 🌲❄️ 🌲❄️ 🌲❄️ 🌲");
+}
+
+exports.printBeautifulChristianHolidaySymbolsWinterII = function() {
+  console.log("🐐👴🐐👴🐐👴🐐👴🐐👴🐐👴🐐👴🐐👴🐐👴🐐👴🐐👴🐐👴🐐");
+}
+
+exports.printBeautifulChristianHolidaySymbolsWinter = function() {
+  console.log("🌲🎅🌲🎅🌲🎅🌲🎅🌲🎅🌲🎅🌲🎅🌲🎅🌲🎅🌲🎅🌲🎅🌲🎅🌲");
 }
